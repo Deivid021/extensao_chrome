f617f9b3ca5d0bfc4703f500e02d8174e90ba808
Deivid021 <deividsestrensantos@gmail.com>
2025-09-18 23:20:47 -0300

    Implementação inicial de um popup para a tela anti-procratinação

--- extension_for_procrastination/content.js
+++ extension_for_procrastination/content.js
@@ -2,21 +2,41 @@
 class antiProcrastination {
     constructor() {
         this.url = window.location.href;
-        this.bloqueados = ["https://classroom.google.com","youtube.com"];
-    console.log(this.url);
-} 
+        this.bloqueados = ["https://classroom.google.com","https://www.youtube.com"];
+}
 
     validaUrl() {
         const urlBloqueada = this.bloqueados.some(url => this.url.startsWith(url));
-    
+
         if (urlBloqueada) {
-            console.log("funcionou, o link da pagina que vc está atualmente está nos bloqueados")
-        } else {
-            console.log("não funcionou");
+            this.mostraPopup();
         }
+    }
+
+    mostraPopup() {
+        const newDiv = document.createElement("div");
+
+        newDiv.innerHTML = `
+            <div style="
+                position: fixed;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                background: rgba(0,0,0,0.9);
+                color: white;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                z-index: 9999;
+            ">
+                <h2>🚫 Este domínio está na lista de bloqueados!</h2>
+            </div>
+        `;
+        
+        document.body.appendChild(newDiv);
     }
 }
 
 const antiProcrastinationApp = new antiProcrastination();
-
 antiProcrastinationApp.validaUrl();
